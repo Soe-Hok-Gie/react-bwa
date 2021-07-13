5f35c005e3b1a11ef9365acca38b6a72530f12d5
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-07-14 01:17:42 +0700

    design template harga coffe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const userLogged ="Fadil"
-  const userLoggedJsx = <strong>"dasar jsx"</strong>
+  // const userLogged ="Fadil"
+  // const userLoggedJsx = <strong>"dasar jsx"</strong>//bisa menambahkan fungsi HTMl
+
 
 
   return (
-    <div className="App">
+    <div class="Parentbox">
+{/* 
      {userLogged}
-     {userLoggedJsx}
+     {userLoggedJsx} */}
+     <div class="Foto">
+       <img src="kopi.jpg"/>
+     </div>
+     <div class="Deskripsi">
+     <p class="Coffe">coffe</p>
+      <h1 class="Title"> Biji Coffe </h1>
+      <p class="Price">IDR 73.400.00</p>
+      <p class="Info">coffe blabla</p>
+           
+           
+    </div>
     </div>
   );
 }
