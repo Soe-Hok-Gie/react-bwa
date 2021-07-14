aebf1f974876a3dde9c0e5f3cdc82370bfebe7dc
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-07-15 01:43:44 +0700

    props pada produk info

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   // const userLogged ="Fadil"
   // const userLoggedJsx = <strong>"dasar jsx"</strong>//bisa menambahkan fungsi HTMl
-
-
   return (
     <div className="Parentbox">
-{/* 
-     {userLogged}
+      <FotoProduk/>
+      <ProdukInfo name="Coffe Arabika"category="hadiah"/>
+    
+     {/* {userLogged}
      {userLoggedJsx} */}
-      
     </div>
   );
 }
 
-function fotoProduk (){
+function FotoProduk (){
   return (
 
     <div className="Foto">
@@ -27,18 +25,18 @@ function fotoProduk (){
   );
 }
 
-function produkInfo (){
-  const category="Coffea arabika";
+function ProdukInfo (props){
+  const {category,name}=props;
   return (
-<div>
+  <div>
   <div className="Deskripsi">
   <p className="Coffe">{category}</p>
-  <h1 className="Title"> Biji Coffe </h1>
+  <h1 className="Title"> {name} </h1>
   <p className="Price">IDR 7.300.000.00</p>
   <p className="Info">Kopi merupakan salah satu komoditas di dunia yang dibudidayakan lebih dari 50 negara. 
   Dua spesies pohon kopi yang dikenal secara umum yaitu Kopi Robusta (Coffea canephora) dan Kopi Arabika (Coffea arabica).</p>              
-</div>
-</div>
+  </div>
+  </div>
   );
 }
 
