627284e5ce6c6e15fa82c703d4e982629fbc140a
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-07-15 00:58:00 +0700

    slicing componen

--- src/App.js
+++ src/App.js
@@ -7,24 +7,38 @@ function App() {
   // const userLogged ="Fadil"
   // const userLoggedJsx = <strong>"dasar jsx"</strong>//bisa menambahkan fungsi HTMl
 
- const category="Coffea arabika"
 
   return (
-    <div class="Parentbox">
+    <div className="Parentbox">
 {/* 
      {userLogged}
      {userLoggedJsx} */}
-       <div class="Foto">
-         <img src="kopi.jpg"/>
-      </div>
-      <div class="Deskripsi">
-      <p class="Coffe">{category}</p>
-      <h1 class="Title"> Biji Coffe </h1>
-      <p class="Price">IDR 7.300.000.00</p>
-      <p class="Info">Kopi merupakan salah satu komoditas di dunia yang dibudidayakan lebih dari 50 negara. 
-      Dua spesies pohon kopi yang dikenal secara umum yaitu Kopi Robusta (Coffea canephora) dan Kopi Arabika (Coffea arabica).</p>              
+      
     </div>
+  );
+}
+
+function fotoProduk (){
+  return (
+
+    <div className="Foto">
+      <img src="kopi.jpg"/>
     </div>
+  );
+}
+
+function produkInfo (){
+  const category="Coffea arabika";
+  return (
+<div>
+  <div className="Deskripsi">
+  <p className="Coffe">{category}</p>
+  <h1 className="Title"> Biji Coffe </h1>
+  <p className="Price">IDR 7.300.000.00</p>
+  <p className="Info">Kopi merupakan salah satu komoditas di dunia yang dibudidayakan lebih dari 50 negara. 
+  Dua spesies pohon kopi yang dikenal secara umum yaitu Kopi Robusta (Coffea canephora) dan Kopi Arabika (Coffea arabica).</p>              
+</div>
+</div>
   );
 }
 
